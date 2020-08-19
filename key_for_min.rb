4abb56bede7b4fe132435d9cf4c11ea4b3c91824
Alma Flores <flores.almalee@gmail.com>
2020-08-19 16:42:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest = 
+  min = ""
+  lowest = Infinity 
   name_hash.each do |key, value|
-    i
+    if value < lowest
+      lowest = value
+      min = key
+    end
+    min
 end
